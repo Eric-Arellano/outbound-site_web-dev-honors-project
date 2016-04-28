660821de7244563b7cc18f5830cc3395c6f7d946
Eric Arellano <ericarellano@me.com>
2016-04-28 11:35:08 -0700

    initial JS for slider

---
+++ javascript/hero-slider.js
@@ -0,0 +1,41 @@
+// Assign each slide an index
+// Create a button for each slider
+//
+// Create function to move slide
+//   - change class of .page--hero by giving appropriate hero--image
+//        - must remove previous hero--image class
+//   - animate transitions
+//     - transition left if chosen slide is < index
+//     - transition right if chosen slide is > index
+//
+// Add timer
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  // ---------------------------------------------------------------------
+  // Setup
+  // ---------------------------------------------------------------------
+
+  var heroContainer = document.querySelector('.slider');
+  var slides = document.querySelectorAll('.slider__slide');
+
+  // ---------------------------------------------------------------------
+  // Move function
+  // ---------------------------------------------------------------------
+
+  function move(newIndex) {
+    
+  }
+
+
+  // ---------------------------------------------------------------------
+  // Create buttons
+  // ---------------------------------------------------------------------
+
+  // ---------------------------------------------------------------------
+  // Add timer
+  // ---------------------------------------------------------------------
+
+
+
+});
