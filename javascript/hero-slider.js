12695b41b645eafa1ba768dba912156945a4acf1
Eric Arellano <ericarellano@me.com>
2016-04-28 12:04:39 -0700

    adds functionality to change hero image

--- javascript/hero-slider.js
+++ javascript/hero-slider.js
@@ -4,6 +4,7 @@
 // Create function to move slide
 //   - change class of .page--hero by giving appropriate hero--image
 //        - must remove previous hero--image class
+//        - maybe toggle class on and off?
 //   - animate transitions
 //     - transition left if chosen slide is < index
 //     - transition right if chosen slide is > index
@@ -16,17 +17,46 @@ document.addEventListener('DOMContentLoaded', function() {
   // Setup
   // ---------------------------------------------------------------------
 
-  var heroContainer = document.querySelector('.slider');
+  var heroContainer = document.querySelector('.page--hero');
   var slides = document.querySelectorAll('.slider__slide');
 
+  var currentIndex = 0;
+  var currentHero = 'hero--alaska';
+
   // ---------------------------------------------------------------------
-  // Move function
+  // Move functions
   // ---------------------------------------------------------------------
 
   function move(newIndex) {
-    
+
+    changeContent(newIndex);
+    animateMove(currentIndex, newIndex);
+
   }
 
+
+    function changeContent(newIndex) {
+
+      // remove current hero--image
+      heroContainer.classList.remove(currentHero);
+
+      // determine new hero--image
+      var newHero = '';
+      if (newIndex === 0) { newHero = 'hero--alaska'; }
+      if (newIndex === 1) { newHero = 'hero--east-coast'; }
+      if (newIndex === 2) { newHero = 'hero--eric'; }
+
+      // add new hero--image
+      heroContainer.classList.add(newHero);
+      currentHero = newHero;
+
+      // change text
+
+    }
+
+    function animateMove(currentIndex, newIndex) {
+
+    }
 
   // ---------------------------------------------------------------------
   // Create buttons
