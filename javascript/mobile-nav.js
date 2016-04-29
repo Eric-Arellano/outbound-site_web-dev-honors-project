2f863f4c6c98ccb125c10211d82ec79c8a07d58f
Eric Arellano <ericarellano@me.com>
2016-04-29 12:24:18 -0700

    adds required jquery file

--- javascript/mobile-nav.js
+++ javascript/mobile-nav.js
@@ -1,11 +1,13 @@
 $(function() {
 
   var $pull = $('.primary-nav__pull');
-  var $menu = $('primary-nav__list');
-  var $menuHeight = $menu.height();
+  var $menu = $('.primary-nav__list');
+  // var $menuHeight = $menu.height();
 
-  $($pull).on('click', function(event) {
+
+  $pull.on('click', function(event) {
     event.preventDefault();
     $menu.slideToggle();
   });
+
 });
