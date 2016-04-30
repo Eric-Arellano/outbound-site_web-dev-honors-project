c2c95feb2ee0895a9b00ccdc755347347d30fb92
Eric Arellano <ericarellano@me.com>
2016-04-30 10:31:58 -0700

    now resizes mast

--- javascript/mobile-nav.js
+++ javascript/mobile-nav.js
@@ -2,12 +2,20 @@ $(function() {
 
   var $pull = $('.primary-nav__pull');
   var $menu = $('.primary-nav__list');
-  // var $menuHeight = $menu.height();
+  var $mast = $('.mast');
 
+  var $menuHeight = $menu.height();
+  var $mastHeight = $mast.height();
 
   $pull.on('click', function(event) {
     event.preventDefault();
+
+    $mast.height($mastHeight + $menuHeight).fadeIn(1400);
+
     $menu.slideToggle();
+
   });
+
+  // TODO: Add reversion after closing menu
 
 });
