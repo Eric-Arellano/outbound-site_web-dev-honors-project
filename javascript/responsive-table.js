749b2afddf246e0010498828b496bcadb17b8df9
Eric Arellano <ericarellano@me.com>
2016-04-30 15:59:55 -0700

    remove error tracing

--- javascript/responsive-table.js
+++ javascript/responsive-table.js
@@ -10,17 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
     headerText.push(currentHeader.textContent.replace(/\r?\n|\r/, ''));
   }
 
-  console.log(headerText);
-
   // assign headers to td
   for (var indexRow = 0; indexRow < tableBody.rows.length; indexRow++) {
 
-    console.log(indexRow);
-    
     for (var indexCol = 0; indexCol < tableBody.rows[indexRow].cells.length;
       indexCol++) {
 
-      console.log(indexCol);
       var currentCell = tableBody.rows[indexRow].cells[indexCol];
       currentCell.setAttribute('data-th', headerText[indexCol]);
     }
