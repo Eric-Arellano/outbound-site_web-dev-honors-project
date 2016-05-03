5eb5593af057421ba6d0fb0f96061be7faef8820
Eric Arellano <ericarellano@me.com>
2016-05-03 11:27:58 -0700

    fixes mobile nav icon changing position

--- javascript/mobile-nav.js
+++ javascript/mobile-nav.js
@@ -12,7 +12,7 @@ $(function() {
 
     // // change mast height
     // // TODO: Get these animations to work
-    var SHRINK_TO_FIT = 1.7;
+    var SHRINK_TO_FIT = 2;
     if ($menu.css('display') === 'none') {
       $mast.height($mastHeight + ($menuHeight / SHRINK_TO_FIT)).animate(4000);
     }
