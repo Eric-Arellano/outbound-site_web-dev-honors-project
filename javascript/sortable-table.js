e833a9a40d5e9c547b11b0948b3adbe43dd426b1
Eric Arellano <ericarellano@me.com>
2016-05-05 15:52:59 -0700

    redo with Duckett code

--- javascript/sortable-table.js
+++ javascript/sortable-table.js
@@ -1,54 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  // ---------------------------------------------------------------------
-  // Set up table
-  // ---------------------------------------------------------------------
-  var tableHeaders = document.querySelectorAll('.table--sortable th');
-  var tableBody = document.querySelector('.table--sortable tbody');
-  var rows = $(tableBody).find('tr').toArray();
+  var $table = $('.table--sortable');
+  var $tbody = $table.find('tbody');
+  var $controls = $table.find('th');
+  var rows = $tbody.find('tr').toArray();
 
-  // add event listener
-  for (var headerIndex = 0; headerIndex < tableHeaders.length; headerIndex++) {
-    var header = tableHeaders[headerIndex];
-    header.addEventListener('click', sort(header, headerIndex), false);
-  }
+  $controls.on('click', function() {
+    var $header = $(this);
+    var sortType = $header.data('sort');
 
-  // ---------------------------------------------------------------------
-  // Sort column
-  // ---------------------------------------------------------------------
-
-  function sort(header, headerIndex) {
-    if (header.classList.contains('sort--ascending') ||
-      header.classList.contains('sort--descending')) {
-      reverseOrder(header);
-      }
-    else {
-      sortAscending(header, headerIndex);
+    // reverse if already sorted
+    if ($header.is('.sort--ascending') || $header.is('.sort--descending')) {
+      $header.toggleClass('sort--ascending  sort--descending');
+      $tbody.append(rows.reverse());
     }
-  }
 
-  function sortAscending(header) {
-    header.classList.add('sort--ascending');
+    // else sort in ascending order
+    else {
+      $header.addClass('sort--ascending');
 
-    // remove sorting of siblings
-    $(header).siblings().remove('sort--ascending  sort--descending');
+      // remove sorting from siblings
+      $header.siblings().removeClass('sort--ascending  sort--descending');
 
-    // sort based off of type
-    var sortType = $(header).data('sort');
-    rows.sort(function(a, b) {
-      a = $(a).find('td').eq(headerIndex).text();
-      b = $(b).find('td').eq(headerIndex).text();
-      return compare[sortType](a, b);
-    });
+      // sort according to type
+      if (compare.hasOwnProperty(order)) {
+        var column = $controls.index(this);
 
-    tableBody.append(rows);
-  }
+        rows.sort(function(a, b) {
+          a = $(a).find('td').eq(column).text();
+          b = $(b).find('td').eq(column).text();
+          return compare[sortType](a, b);
+        });
+      }
 
-  function reverseOrder(header){
-    header.classList.toggle('sort--ascending');
-    header.classList.toggle('sort--descending');
-    tableBody.append(rows.reverse());
-  }
+      $tbody.append(rows.reverse());
+    }
+  });
 
   // ---------------------------------------------------------------------
   // Compare functions
@@ -88,3 +75,53 @@ document.addEventListener('DOMContentLoaded', function() {
   };
 
 });
+//
+// // ---------------------------------------------------------------------
+// // Set up table
+// // ---------------------------------------------------------------------
+// var tableHeaders = document.querySelectorAll('.table--sortable th');
+// var tableBody = document.querySelector('.table--sortable tbody');
+// var rows = $(tableBody).find('tr').toArray();
+//
+// // add event listener
+// for (var headerIndex = 0; headerIndex < tableHeaders.length; headerIndex++) {
+//   var header = tableHeaders[headerIndex];
+//   header.addEventListener('click', sort(header, headerIndex), false);
+// }
+//
+// // ---------------------------------------------------------------------
+// // Sort column
+// // ---------------------------------------------------------------------
+//
+// function sort(header, headerIndex) {
+//   if (header.classList.contains('sort--ascending') ||
+//     header.classList.contains('sort--descending')) {
+//     reverseOrder(header);
+//     }
+//   else {
+//     sortAscending(header, headerIndex);
+//   }
+// }
+//
+// function sortAscending(header) {
+//   header.classList.add('sort--ascending');
+//
+//   // remove sorting of siblings
+//   $(header).siblings().remove('sort--ascending  sort--descending');
+//
+//   // sort based off of type
+//   var sortType = $(header).data('sort');
+//   rows.sort(function(a, b) {
+//     a = $(a).find('td').eq(headerIndex).text();
+//     b = $(b).find('td').eq(headerIndex).text();
+//     return compare[sortType](a, b);
+//   });
+//
+//   tableBody.append(rows);
+// }
+//
+// function reverseOrder(header){
+//   header.classList.toggle('sort--ascending');
+//   header.classList.toggle('sort--descending');
+//   tableBody.append(rows.reverse());
+// }
